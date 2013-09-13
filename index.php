f5d47cfa62d5128cb6933fa6049af638f79cf516
Emily Keuthen <ekeuthen@gmail.com>
2013-09-13 14:06:31 -0400

    Adding first version of P1

--- index.php
+++ index.php
@@ -1,1 +1,21 @@
-Welcome to P1!
+<!DOCTYPE html>
+<!--Emily Keuthen ekeuthen@gmail.com CSCI E-15 P1 September 2013-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   		<title>Emily CSCI E-15 P1</title>
+   		<link rel="stylesheet" type="text/css" href="emily.css">
+  	</head>
+  	<body>
+	  	<div id="nametag">
+	    	<h1>HELLO</h1>
+	    	<h2>my name is</h2>
+	    	<br />
+	    	<div id="personalinfo">
+		    	<h1>Emily</h1>
+		    	<p>I'm psyched to learn more about web development with this class.  After avoiding anything computer-related for most of my life, I feel lucky to have the opportunity to remedy this while working at Harvard.  At work I think a lot about how to design, test, and document systems so I'm eager to gain insight into the development process.</p>
+		    	<p>Fun fact: This HTML5 document was authored using <a href="http://www.sublimetext.com/" target="blank">Sublime Text</a>!</p>
+		    </div>
+    	</div>
+  	</body>
+</html>
