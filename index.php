91d2e1927d7842be3f9006d99c3c86cf098b2d6e
Emily Keuthen <ekeuthen@gmail.com>
2013-09-13 14:12:23 -0400

    Modified css link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
    		<title>Emily CSCI E-15 P1</title>
-   		<link rel="stylesheet" type="text/css" href="emily.css">
+   		<link rel="stylesheet" type="text/css" href="Emily.css">
   	</head>
   	<body>
 	  	<div id="nametag">
